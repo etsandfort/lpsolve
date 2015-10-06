169491188ea740b401f413604ad224fcae82574e
etsandfort <etsandfort@gmail.com>
2015-10-05 23:47:33 -0500

    Getting Started

---
+++ LpModel.py
@@ -0,0 +1,10 @@
+__author__ = 'Elliot, David, Jacob'
+class LpModel
+    def __init__(self, numConstraints, numVars):
+    def setColumn(self, colNum, values):
+    def setObjfn(self, values):
+    def setConstrType(self,constrType):
+    def setRHS(self, values):
+    def lpControl(self, minmax):
+    def printLp(self):
+        #matrices: constraints, rhs, objectivefunction
