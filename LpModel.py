72bdad127d197e2d93e26ad73c2462c0405f8665
WRCTG <735125909@qq.com>
2015-10-06 13:20:05 -0500

    input
    I just set everything as a matrix except for the listOfColumns cause I
    am not sure if creating a matrix of matrix is a good idea lol.

--- LpModel.py
+++ LpModel.py
@@ -1,10 +1,31 @@
 __author__ = 'Elliot, David, Jacob'
-class LpModel
+import numpy
+class LpModel:
     def __init__(self, numConstraints, numVars):
+        self.numConstraints = numConstraints
+        self.numVars = numVars
+        self.listOfColumns = []
     def setColumn(self, colNum, values):
+        self.listOfColumns[colNum]= numpy.matrix(values)
     def setObjfn(self, values):
+        self.objfn = numpy.matrix(values)
     def setConstrType(self,constrType):
+        if constrType == '<=' :
+            self.ConstrType = 1
+        if constrType == '>=':
+            self.ConstrType = 2
+        if constrType == '<':
+            self.ConstrType = 3
+        if constrType == '>':
+            self.ConstrType = 4
+        if constrType == '=':
+            self.ConstrType = 5
     def setRHS(self, values):
+        self.rhs = numpy.matrix(values)
     def lpControl(self, minmax):
+        if minmax == 'min':
+            self.lpControl=0
+        if minmax == 'max':
+            self.lpControl=1
     def printLp(self):
         #matrices: constraints, rhs, objectivefunction
